bcf15e9d595df3e5d6df49c854a9f5d1a7ef7f9c
Keerthan M <kituk2004@gmail.com>
2026-06-23 22:30:42 +0530

    Update .gitignore to include env directory and modify Django settings to add new apps and middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foodordering',
+    'rest_framework',
+    'corsheaders',
 ]
 
+CORS_ALLOWED_ORIGIN = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
