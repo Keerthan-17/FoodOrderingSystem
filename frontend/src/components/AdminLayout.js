4c45b402a94bb3753a948db75ac386aff98081b9
Keerthan M <kituk2004@gmail.com>
2026-07-01 12:33:57 +0530

    Implement responsive sidebar toggle in AdminLayout and update CSS for page content layout

--- frontend/src/components/AdminLayout.js
+++ frontend/src/components/AdminLayout.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/admin.css'
 import AdminSidebar from './AdminSidebar'
 import AdminHeader from './AdminHeader'
 
 const AdminLayout = ({children}) => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(true)
+  useEffect(() => {
+    const handleResize = () =>{
+      if (window.innerWidth < 768){
+        setSidebarOpen(false)
+      }
+      else{
+        setSidebarOpen(true)
+      }
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+  
   return (
     <div className='d-flex'>
-      <AdminSidebar/>
+      {sidebarOpen && <AdminSidebar/>}
       
-      <div id='page-content-wrapper' className='w-100'>
+      <div id='page-content-wrapper' className={`flex-grow-1 ${sidebarOpen ? 'with-sidebar' : 'full-sidebar'}`}>
         <AdminHeader/>
         <div className='container-fluid mt-4'>
           {children}
