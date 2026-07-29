df23cafc30900bc82e770d6f37c3dc4c9177eecb
Keerthan-17 <kituk2004@gmail.com>
2026-07-29 17:07:35 +0530

    Reset category name after successful addition in AddCategory component

--- frontend/src/pages/AddCategory.js
+++ frontend/src/pages/AddCategory.js
@@ -21,6 +21,7 @@ const AddCategory = () => {
     
         if (response.status===201) {
           toast.success(data.message)
+          setCategoryName('')
         }
         else {
           toast.error("Something went wrong!!")
