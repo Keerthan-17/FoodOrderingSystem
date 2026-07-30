b8d347af3c75bf47d0006ed378f3b4fc37a9a598
Keerthan-17 <kituk2004@gmail.com>
2026-07-30 10:20:36 +0530

    Add ManageFood component and update routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import AdminLogin from './pages/AdminLogin';
-import AdminDashboard from './pages/AdminDashboard';
-import AddCategory from './pages/AddCategory';
-import ManageCategory from './pages/ManageCategory';
-import AddFood from './pages/AddFood';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import AddCategory from "./pages/AddCategory";
+import ManageCategory from "./pages/ManageCategory";
+import AddFood from "./pages/AddFood";
+import ManageFood from "./pages/ManageFood";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/admin-login' element={<AdminLogin/>}></Route>
-        <Route path='/admin-dashboard' element={<AdminDashboard/>}></Route>
-        <Route path='/add-category' element={<AddCategory/>}></Route>
-        <Route path='/manage-category' element={<ManageCategory/>}></Route>
-        <Route path='/add-food' element={<AddFood/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/admin-login" element={<AdminLogin />}></Route>
+        <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+        <Route path="/add-category" element={<AddCategory />}></Route>
+        <Route path="/manage-category" element={<ManageCategory />}></Route>
+        <Route path="/add-food" element={<AddFood />}></Route>
+        <Route path="/manage-food" element={<ManageFood />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/ManageFood.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ManageFood = () => {
+  return (
+    <div>Manage Food</div>
+  )
+}
+
+export default ManageFood
