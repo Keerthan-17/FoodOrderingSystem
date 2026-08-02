d76895ce748ee98230d9100ad958f76921788ba8
Keerthan M <kituk2004@gmail.com>
2026-08-02 22:20:12 +0530

    added few  categories and food items.

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,46 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import PublicLayout from '../components/PublicLayout'
-import { Link, useLocation } from 'react-router-dom'
-import '../styles/rare.css'
+import React, { useEffect, useState } from "react";
+import PublicLayout from "../components/PublicLayout";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/rare.css";
 
 const SearchPage = () => {
-  
-  const query = new URLSearchParams(useLocation().search).get('q') || '';
+  const query = new URLSearchParams(useLocation().search).get("q") || "";
   const [foods, setFoods] = useState([]);
 
-  useEffect(()=>{
-    if (query){
+  useEffect(() => {
+    if (query) {
       fetch(`http://127.0.0.1:8000/api/foods_search/?q=${query}`)
-      .then(res=> res.json())
-      .then(data => {
-        setFoods(data)
-      })
+        .then((res) => res.json())
+        .then((data) => {
+          setFoods(data);
+        });
     }
-  },[query])
+  }, [query]);
 
   return (
     <PublicLayout>
-      <div className='container py-4'>
-        <h3 className='text-primary text-center'>Results for: "{query}"</h3>
-        <div className='row mt-4'>
+      <div className="container py-4">
+        <h3 className="text-primary text-center">Results for: "{query}"</h3>
+        <div className="row mt-4">
           {foods.length === 0 ? (
-            <p className='text-center text-muted'>
-              No Foods Found!!
-            </p>
+            <p className="text-center text-muted">No Foods Found!!</p>
           ) : (
-            foods.map((food,index)=>(
-              <div className='col-md-4 mb-4'>
-                <div className='card hovereffect'>
-                  <img src={`http://127.0.0.1:8000${food.image}`} className='card-img-top' style={{height: "180px"}} alt='Food Img'/>
-                  <div className='card-body'>
-                    <h5 className='card-title'>
+            foods.map((food, index) => (
+              <div className="col-md-4 mb-4">
+                <div className="card hovereffect">
+                  <img
+                    src={`http://127.0.0.1:8000${food.image}`}
+                    className="card-img-top"
+                    style={{ height: "180px" }}
+                    alt="Food Img"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
                       <Link to="#">{food.item_name}</Link>
                     </h5>
-                    <p className='card-text text-muted'>{food.item_description?.slice(0,40)}...</p>
-                    <div className='d-flex justify-content-between align-items-center'>
-                      <span className='fw-bold'>₹ {food.item_price}</span>
-                      <Link className='btn btn-outline-primary btn-sm'>
-                      <i className='fas fa-shopping-basket me-1'></i>Order Now
+                    <p className="card-text text-muted">
+                      {food.item_description?.slice(0, 40)}...
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <span className="fw-bold">₹ {food.item_price}</span>
+                      <Link className="btn btn-outline-primary btn-sm">
+                        <i className="fas fa-shopping-basket me-1"></i>Order Now
                       </Link>
                     </div>
                   </div>
@@ -51,8 +55,7 @@ const SearchPage = () => {
         </div>
       </div>
     </PublicLayout>
-    
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
